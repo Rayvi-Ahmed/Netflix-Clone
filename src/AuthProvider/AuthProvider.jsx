705a50d92ca990726ab8b2cf1faa34b786405d66
Rayvi-Ahmed <rayvi73@gmail.com>
2023-12-13 09:42:16 +0600

    authentication authprovider setup

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,60 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword,GoogleAuthProvider, GithubAuthProvider, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import app from '../Firebase/Firebase.config';
+
+export const AuthContext=createContext()
+const auth= getAuth(app)
+const Googleprovider = new GoogleAuthProvider();
+
+
+
+const AuthProvider = ({children}) => {
+    const [user,setUser]=useState()
+    const [loading,setLoading]=useState(false)
+
+    const SignUp=(email,password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+      
+    }
+
+    const googleSignUp=()=>{
+        setLoading(true)
+        return GithubAuthProvider(auth,Googleprovider)
+
+    }
+
+    const LogIn=(email,password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    useEffect(()=>{
+
+    const unsubscribe=onAuthStateChanged(auth,(currentUser)=>{
+        console.log('this is inside the user',currentUser)
+        setUser(user)
+        setLoading(true)
+    })
+
+    return ()=>{
+        unsubscribe()
+    }
+    },[])
+
+    const authInfo={
+        SignUp,
+        LogIn,
+        googleSignUp,
+        user,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router/Router.jsx'
+import AuthProvider from './AuthProvider/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-   <RouterProvider router={router}>
-   <App />
+<AuthProvider>
+<RouterProvider router={router}>
    </RouterProvider>
+</AuthProvider>
   </React.StrictMode>,
 )
